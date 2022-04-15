1f7fb8ca068e7c34b19b01a0687883181f244830
codyccarmona <76072605+codyccarmona@users.noreply.github.com>
2022-04-14 20:20:02 -0400

    Working implementation for single test input file

--- dijkstra.h
+++ dijkstra.h
@@ -1,12 +1,12 @@
-#include <deque>
+#include <vector>
 #include <math.h>
 #include <string>
 
 struct Node{
-    std::string id = "";
     double estimate = __DBL_MAX__;
-    //int predecessor;
     std::string adjacent_info = "";
+    Node* predecessor = nullptr;
+    std::string id = "";
 };
 
 int left(int i, bool start_index_zero = true){
@@ -21,18 +21,19 @@ int parent(int i){
     return floor(i / 2);
 }
 
-void min_heapify(std::deque<Node>& nodes, int i){
+void min_heapify(std::vector<Node*>& nodes, int i){
     int smallest;
     auto l = left(i);
     auto r = right(i);
 
-    if(l < nodes.size() && (nodes[l].estimate < nodes[i].estimate))
+    if(l < nodes.size() && (nodes[l]->estimate < nodes[i]->estimate))
         smallest = l;
     else
         smallest = i;
 
-    if(r < nodes.size() && (nodes[r].estimate < nodes[smallest].estimate))
+    if(r < nodes.size() && (nodes[r]->estimate < nodes[smallest]->estimate))
         smallest = r;
+
     if(smallest != i){
         auto temp = nodes[i];
         nodes[i] = nodes[smallest];
@@ -41,17 +42,19 @@ void min_heapify(std::deque<Node>& nodes, int i){
     }
 }
 
-Node extract_min(std::deque<Node>& nodes){
+Node* extract_min(std::vector<Node*>& nodes){
     if(nodes.size() < 0)
         std::__throw_underflow_error("heap underflow");
-    Node max = nodes[0];
+    auto max = nodes[0];
     nodes[0] = nodes[nodes.size() - 1];
+    nodes[nodes.size() - 1] = nullptr;
     nodes.pop_back();
     min_heapify(nodes, 0);
     return max;
 }
 
-void decrease_key(std::deque<Node>& nodes, int i){
+/*
+void decrease_key(std::vector<Node*>& nodes, int i){
     if(nodes[i].estimate < nodes[0].estimate)
         std::__throw_invalid_argument("new key is smaller than current key");
 
@@ -62,8 +65,8 @@ void decrease_key(std::deque<Node>& nodes, int i){
         i = parent(i);
     }
 }
-
-void build_min_heap(std::deque<Node>& nodes){
+*/
+void build_min_heap(std::vector<Node*>& nodes){
     for(int i = floor(nodes.size()); i >= 0; i--)
         min_heapify(nodes, i);
 }
--- shortestpath.cpp
+++ shortestpath.cpp
@@ -2,59 +2,87 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
-#include <vector>
-#include <set>
+#include <map>
+#include <deque>
 using namespace std;
 
 string src_file_name;
 string start_node_id;
 string end_node_id;
 
-void read_into_nodes(deque<Node>&, string, string);
-vector<Node> dijkstra(deque<Node>&);
+void read_into_nodes(vector<Node*>&, map<string, Node*>&);
+void dijkstra(vector<Node*>&, map<string, Node*>&);
 
 
 int main(int argc, char* argv[]){
-    deque<Node> src_nodes;
-    vector<Node> result_nodes;
+    vector<Node*> nodes;
+    map<string, Node*> node_addresses;
 
     src_file_name = argv[1];
     start_node_id = argv[2];
     end_node_id = argv[3];
 
-    read_into_nodes(src_nodes);
-    build_min_heap(src_nodes);
-    dijkstra(src_nodes, result_nodes);
+    read_into_nodes(nodes, node_addresses);
+    build_min_heap(nodes);
+    dijkstra(nodes, node_addresses);
 
+    Node* node_itr = node_addresses.find(end_node_id)->second;
+    string path = "";
+    double weight = node_itr->estimate;
+
+    while(node_itr->id != start_node_id){
+        path += node_itr->id;
+        path += " ";
+        node_itr = node_itr->predecessor;
+    }
+
+    std::cout << path << " " << node_itr->id << std::endl << weight << std::endl;
+    node_itr = nullptr;
+    
+    
+    
+    nodes.erase(nodes.begin(), nodes.end());
+    node_addresses.erase(node_addresses.begin(), node_addresses.end());
+     
     return 0;
 }
 
-void read_into_nodes(deque<Node>& nodes){
+void read_into_nodes(vector<Node*>& nodes, map<string, Node*>& src_nodes){
     ifstream file(src_file_name);
-    string line;
-
-    // Size nodes to node count read from first line. If a line is not read, keep it empty
-    //      to avoid performing next loop
-    nodes = (getline(file, line)) ? deque<Node>(stoi(line), Node{}) : deque<Node>(0);
-
-    for(auto& n : nodes){
+    map<string, Node*> node_addresses;
+    string line = "";
+    int node_count = (getline(file, line)) ? stoi(line) : 0;
+    
+    for(auto index = 0; index < node_count; index++){
+        string node_id;
+        nodes.push_back(new Node{});
         getline(file, line);
         stringstream ss(line);
-        ss >> n.id;
-        getline(ss, n.adjacent_info);
-        if(n.id == start_node_id)
-            n.estimate = 0.0;
+
+        ss >> nodes[index]->id;
+        getline(ss, nodes[index]->adjacent_info);
+
+        if(nodes[index]->id == start_node_id)
+            nodes[index]->estimate = 0.0;
+
+        src_nodes.insert(pair<string, Node*>(nodes[index]->id, nodes[index]));
     }
 
     file.close();
 }
 
-void dijkstra(deque<Node>& nodes, vector<Node>& result){
+void dijkstra(vector<Node*>& nodes, map<string, Node*>& node_addresses){
     while(!nodes.empty()){
         auto current_node = extract_min(nodes);
-
-
-        if(current_node.id == start_node_id)
-            return;
+        stringstream ss(current_node->adjacent_info);
+        string id;
+        double weight;
+        while(ss >> id >> weight){
+            auto adj_node = node_addresses.find(id);
+            if(adj_node->second->estimate > (current_node->estimate + weight)){
+                adj_node->second->estimate = (current_node->estimate + weight);
+                adj_node->second->predecessor = current_node;
+            }
+        }
     }
 }
